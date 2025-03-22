57f057774de90e2f631f4e5f7af513c99411c93b
Lans12345 <olanalans12345@gmail.com>
2025-03-22 10:03:45 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:attendify/firebase_options.dart';
 import 'package:attendify/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'attendify-97737',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
